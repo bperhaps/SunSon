4196833149b2715b25e45bb4fe1ae98a41c49e1c
Minsung <sns2831@gmail.com>
2021-05-19 12:42:37 +0900

    Initial commit

---
+++ src/main/java/com/woowahan/techcourse/SunSon/SunSonApplication.java
@@ -0,0 +1,13 @@
+package com.woowahan.techcourse.SunSon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunSonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunSonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woowahan/techcourse/SunSon/SunSonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowahan.techcourse.SunSon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunSonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
